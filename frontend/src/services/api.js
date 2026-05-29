b9ea30cae3ccf1668a164d0354f206a5b9e37efb
Santosh-ai1312 <santoshkumarbandi1312@gmail.com>
2026-05-29 13:47:06 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://sales-savvy-7ivm.onrender.com/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 // Attach JWT token to every request
